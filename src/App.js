d5266a487bc32e5f2fed9ada87fa746bf5c8b233
TakeMos <kangh0423@gmail.com>
2023-10-24 10:49:24 +0900

    text

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    
+  }
 }
 console.log("a")
+console.log(MissionUtils.Random.pickNumberInList([1, 2, 3, 4 ,5 ,6, 7, 8, 9]));
 export default App;
